cd7196651b02759105f27760f96f2854fbda5f4e
roundyz <roundyz@MR-DEV-02.(none)>
2012-03-23 12:06:14 +0000

    setters and getters are now seperate. Added protection for setters

--- ClassMaker.php
+++ ClassMaker.php
@@ -14,23 +14,38 @@ function upperCaseFirstLetter($name){
 }
 
 
-function makeGetterAndSetter($in){
-	echo "\n";
-	echo "\tpublic function get".upperCaseFirstLetter($in). "()\n";
-	echo "\t{\n";
-	echo "\t\t".'return $this->';
-	echo $in;
-	echo ";";
-	echo "\n\t}\n";
-	echo "\n\n";
-	echo "\tpublic function set".upperCaseFirstLetter($in). "( $".$in.' )';
-	echo "\n\t{\n\t\t";
-	echo '$this->';
-	echo $in;
-	echo " = ";
-	echo "$".$in;
-	echo ";\n\t}\n";
-	echo "\n";
+function makeGetter($in)
+{
+	$return ="";
+	$return.= "\n";
+	$return.= "\tpublic function get".upperCaseFirstLetter($in). "()\n";
+	$return.= "\t{\n";
+	$return.= "\t\t".'return $this->';
+	$return.= $in;
+	$return.= ";";
+	$return.= "\n\t}\n";
+	$return.= "\n";
+	return $return;
+}
+
+function makeSetter($in)
+{
+	$return = "";
+	$return.= "\n";
+	$return.= "\tpublic function set".upperCaseFirstLetter($in). "( $".$in.' )';
+	$return.= "\n\t{\n\t\t";
+	$return.= sprintf("$%sIsOK = isset($%s) && $%s != NULL;",$in,$in,$in);
+	$return .= "\n";
+	$return.= sprintf("\t\tif ( $%sIsOK )",$in);
+	$return.= "\n\t\t{\n\t\t\t";
+	$return.= '$this->';
+	$return.= $in;
+	$return.= " = ";
+	$return.= "$".$in;
+	$return.= ";\n\t\t}\n";
+	$return.= "\n\t}\n";
+	$return.= "\n";
+	return $return;
 }
 
 
@@ -68,16 +83,31 @@ function makeConstructor($in){
 		makeInstanceVar($argv[$i+3]);
 	}
 
+	$getters = array();
+	$setters = array();
 	for ($i=0;$i<=$varCount;$i++){
-		makeGetterAndSetter($argv[$i+3]);
+		$getters[] = makeGetter($argv[$i+3]);
+		$setters[] = makeSetter($argv[$i+3]);
 	}
 	
+
+
+	foreach($getters as $getter)
+	{
+		echo $getter;
+	}
+
+	foreach($setters as $setter)
+	{
+		echo $setter;
+	}
+
 	
 	echo "\n";
 
 
 	echo "\tpublic function __toString()
-			{\n\t\t\$out='';\n";
+	{\n\t\t\$out='';\n";
 	for ($i=0;$i<=$varCount;$i++){
 		echo "\t\t\$out.=\$" .($argv[$i+3]).";\n";
 	}
